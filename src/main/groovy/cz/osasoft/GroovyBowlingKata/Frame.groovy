06278544cf2a76b007822b7780d945eaf7e330e1
Oscar Hernandez <OsaSofto@gmail.com>
2016-07-06 15:29:53 +0100

    Added frame class + spec

---
+++ src/main/groovy/cz/osasoft/GroovyBowlingKata/Frame.groovy
@@ -0,0 +1,8 @@
+package cz.osasoft.GroovyBowlingKata
+
+/**
+ * Created by OsaSoft on 06/07/16.
+ */
+class Frame {
+
+}
---
+++ src/test/groovy/cz/osasoft/GroovyBowlingKata/FrameTest.groovy
@@ -0,0 +1,12 @@
+package cz.osasoft.GroovyBowlingKata
+
+import spock.lang.Specification
+
+/**
+ * Created by oscar on 06/07/16.
+ */
+class FrameTest extends Specification {
+    def setup(){}
+
+    def cleanup(){}
+}
