437efa6f95c98da5fe5e776f9e53d4c47a17c929
Oscar Hernandez <OsaSofto@gmail.com>
2016-07-06 15:35:02 +0100

    frame tests pass

--- src/main/groovy/cz/osasoft/GroovyBowlingKata/Frame.groovy
+++ src/main/groovy/cz/osasoft/GroovyBowlingKata/Frame.groovy
@@ -7,17 +7,18 @@ class Frame {
     def rolls = new int[2]
 
     def getFirstRoll(){
-
+        rolls[0]
     }
 
     def setFirstRoll(pins){
+        rolls[0] = pins
     }
 
     def getSecondRoll(){
-
+        rolls[1]
     }
 
     def setSecondRoll(pins){
-
+        rolls[1] = pins
     }
 }
