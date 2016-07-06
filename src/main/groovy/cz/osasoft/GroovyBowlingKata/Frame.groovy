03349d12cdc873afd30a7b159a19871dbb4a56d3
Oscar Hernandez <OsaSofto@gmail.com>
2016-07-06 15:34:05 +0100

    test set rolls in frame

--- src/main/groovy/cz/osasoft/GroovyBowlingKata/Frame.groovy
+++ src/main/groovy/cz/osasoft/GroovyBowlingKata/Frame.groovy
@@ -4,5 +4,20 @@ package cz.osasoft.GroovyBowlingKata
  * Created by OsaSoft on 06/07/16.
  */
 class Frame {
+    def rolls = new int[2]
 
+    def getFirstRoll(){
+
+    }
+
+    def setFirstRoll(pins){
+    }
+
+    def getSecondRoll(){
+
+    }
+
+    def setSecondRoll(pins){
+
+    }
 }
--- src/test/groovy/cz/osasoft/GroovyBowlingKata/FrameTest.groovy
+++ src/test/groovy/cz/osasoft/GroovyBowlingKata/FrameTest.groovy
@@ -6,7 +6,20 @@ import spock.lang.Specification
  * Created by oscar on 06/07/16.
  */
 class FrameTest extends Specification {
-    def setup(){}
+    Frame f
+
+    def setup(){
+        f = new Frame()
+    }
 
     def cleanup(){}
+
+    def "Set rolls"(){
+        when:
+            f.firstRoll = 3
+            f.secondRoll= 2
+        then:
+            f.firstRoll == 3
+            f.secondRoll == 2
+    }
 }
