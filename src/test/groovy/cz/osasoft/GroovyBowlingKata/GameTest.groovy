6dac5eadc8e501dddad17d6be303365ee2630440
Oscar Hernandez <oscar.hernandez@secretescapes.com>
2016-07-06 13:21:37 +0100

    Blank Game class and spec

---
+++ src/test/groovy/cz/osasoft/GroovyBowlingKata/GameTest.groovy
@@ -0,0 +1,11 @@
+package cz.osasoft.GroovyBowlingKata
+
+import spock.lang.Specification
+
+/**
+ * Created by OsaSoft on 06/07/16.
+ */
+class GameTest extends Specification {
+    def setup(){}
+    def cleanup(){}
+}
